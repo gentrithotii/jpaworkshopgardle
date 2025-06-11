6a13f0edab6397e969437acbae889bc090d7b444
Gentrithotii <Gentrithoti@hotmail.com>
2025-06-11 18:00:03 +0200

    Part one done and working

--- src/main/java/com/gentrit/jpaworkshopgardle/entity/Details.java
+++ src/main/java/com/gentrit/jpaworkshopgardle/entity/Details.java
@@ -1,7 +1,14 @@
 package com.gentrit.jpaworkshopgardle.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Details {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
 }
