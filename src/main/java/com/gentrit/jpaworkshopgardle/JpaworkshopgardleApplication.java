62a55432fa922ded29f5f9be3e553d1deceb3d2d
Gentrithotii <Gentrithoti@hotmail.com>
2025-06-11 17:23:11 +0200

    Initial commit

---
+++ src/main/java/com/gentrit/jpaworkshopgardle/JpaworkshopgardleApplication.java
@@ -0,0 +1,13 @@
+package com.gentrit.jpaworkshopgardle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaworkshopgardleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaworkshopgardleApplication.class, args);
+    }
+
+}
