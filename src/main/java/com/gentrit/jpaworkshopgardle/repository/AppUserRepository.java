3cc7fbc906990d340c72803837088df6d76a2156
Gentrithotii <Gentrithoti@hotmail.com>
2025-06-12 21:21:58 +0200

    Smoll updates

--- src/main/java/com/gentrit/jpaworkshopgardle/repository/AppUserRepository.java
+++ src/main/java/com/gentrit/jpaworkshopgardle/repository/AppUserRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+
     Optional<AppUser> findAppUserByUsername(String username);
 
     List<AppUser> findAppUserByRegDateBetween(LocalDate regDateAfter, LocalDate regDateBefore);
--- src/main/java/com/gentrit/jpaworkshopgardle/repository/DetailsRepository.java
+++ src/main/java/com/gentrit/jpaworkshopgardle/repository/DetailsRepository.java
@@ -4,12 +4,12 @@ import com.gentrit.jpaworkshopgardle.entity.Details;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DetailsRepository extends CrudRepository<Details, Long> {
+
     Optional<Details> findDetailsByEmail(String email);
 
     List<Details> findDetailsByNameContains(String name);
